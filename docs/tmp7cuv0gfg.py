3c35525a91090c42df3bf98f2c39ca2b63159ba0
Anubhav Mohandas <anubhav.manav147@gmail.com>
2026-07-05 16:46:29 +0530

    Remove throwaway MCP failure-injection test fixtures (never meant to be committed)

--- docs/tmp7cuv0gfg.py
+++
@@ -1,8 +0,0 @@
-
-import sys, time
-print("DEBUG: server booting, this is not JSON-RPC", flush=True)
-print("{not: valid, json", flush=True)
-time.sleep(0.2)
-print("another garbage line", flush=True)
-sys.stdin.readline()
-time.sleep(2)
